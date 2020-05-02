861b7096c805a903caec57d7ca9712e45b4239a0
coatardbul <36257640+coatardbul@users.noreply.github.com>
2020-05-02 17:50:57 +0800

    优化代码

--- src/main/java/com/coatardbul/river/RiverApplication.java
+++ src/main/java/com/coatardbul/river/RiverApplication.java
@@ -3,13 +3,11 @@ package com.coatardbul.river;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @MapperScan("com.coatardbul.river.mapper")
 @SpringBootApplication
-@EnableEurekaServer
 public class RiverApplication {
 
     public static void main(String[] args) {
